744310324da08b2ad953a499fe9f8a77bc102570
Daniel <daanf@live.nl>
2023-01-31 09:28:34 +0100

    another commit

---
+++ .storybook/main.js
@@ -0,0 +1,19 @@
+const path = require("path");
+module.exports = {
+    stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+    addons: [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/preset-create-react-app",
+        "@storybook/addon-interactions",
+        "./addon/preset.js",
+    ],
+    framework: {
+        name: "@storybook/react-webpack5",
+        options: {},
+    },
+    docs: {
+        autodocs: "tag",
+    },
+    staticDirs: ["../public"],
+};
